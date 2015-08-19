632d25d482b00cc9b62feda50a39019a02d5bf3c
Mithgol the Webmaster <getgit@mithgol.ru>
2015-08-19 21:33:21 +0300

    the CLI script accepts an optional log file name

--- rules-fe2hpt.js
+++ rules-fe2hpt.js
@@ -7,13 +7,20 @@ params.shift(); // 'node'
 params.shift(); // 'rules-fe2hpt'
 
 var rusMode = false;
+var logFile = null;
 params = params.filter(function(nextParam){
    if( nextParam.toLowerCase() === '--rus' ){
       rusMode = true;
+      return false;
+   } else if( nextParam.indexOf('--log=') === 0 ){
+      logFile = nextParam.slice('--log='.length);
       return false;
    }
 
    return true;
 });
 
-thisAPI(process.cwd(), { rusMode: rusMode });
+thisAPI(process.cwd(), {
+   rusMode: rusMode,
+   logFile: logFile
+});
